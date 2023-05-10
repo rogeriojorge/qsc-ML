116ea1ce355129fdb74490f1dab6d90651ce3154
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2023-05-11 00:21:43 +0100

    better nn 2

--- nn_qsc2.py
+++ nn_qsc2.py
@@ -5,11 +5,10 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 import matplotlib.pyplot as plt
-from sklearn.model_selection import KFold, train_test_split
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import tensorflow as tf
-from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Dense, Dropout, BatchNormalization, LeakyReLU, Conv1D, MaxPooling1D, Flatten
+from tensorflow.keras.layers import Dense, Dropout, BatchNormalization, LeakyReLU, Conv1D, MaxPooling1D, Flatten
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.losses import Huber
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
@@ -21,14 +20,16 @@ params = {
     'nfp': 2,
     'model': 'cnn',
     'optimizer': Adam,
-    'learning_rate': 0.006,
-    'epochs': 50,
+    'learning_rate': 0.01,
+    'epochs': 150,
     'batch_size': 512,
-    'early_stopping_patience': 10,
+    'early_stopping_patience': 100,
+    'test_size': 0.2,
+    'random_state': 42
 }
 
 def build_neural_network(input_shape, output_shape):
-    inputs = Input(shape=input_shape)
+    inputs = tf.keras.Input(shape=input_shape)
     x = Dropout(0.5)(inputs)
     x = Dense(512, kernel_initializer='he_normal')(x)
     x = LeakyReLU()(x)
@@ -43,10 +44,10 @@ def build_neural_network(input_shape, output_shape):
     x = BatchNormalization()(x)
     x = Dropout(0.1)(x)
     outputs = Dense(output_shape)(x)
-    return Model(inputs=inputs, outputs=outputs)
+    return tf.keras.Model(inputs=inputs, outputs=outputs)
 
 def build_cnn(input_shape, output_shape):
-    inputs = Input(shape=input_shape)
+    inputs = tf.keras.Input(shape=input_shape)
     x = Conv1D(32, kernel_size=3, activation='relu')(inputs)
     x = MaxPooling1D(pool_size=2)(x)
     x = Conv1D(64, kernel_size=2, activation='relu')(x)
@@ -54,93 +55,88 @@ def build_cnn(input_shape, output_shape):
     x = Dense(128, activation='relu')(x)
     x = Dropout(0.1)(x)
     outputs = Dense(output_shape)(x)
-    return Model(inputs=inputs, outputs=outputs)
+    return tf.keras.Model(inputs=inputs, outputs=outputs)
 
-def main():
-    if len(sys.argv) > 1:
-        params['nfp'] = int(sys.argv[1])
+def preprocess_data(X_train, X_test, Y_train, Y_test, params):
+    scaler_x = StandardScaler().fit(X_train)
+    X_train = scaler_x.transform(X_train)
+    X_test = scaler_x.transform(X_test)
 
-    this_path = str(Path(__file__).parent.resolve())
-    general_results_path = os.path.join(this_path, params['results_path'])
-    results_path = os.path.join(general_results_path, f'nfp{params["nfp"]}')
-    os.makedirs(results_path, exist_ok=True)
+    if params['model'] == 'cnn':
+        X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
+        X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
+        input_shape = (X_train.shape[1], 1)
+    else:
+        input_shape = X_train.shape[1]
 
-    filename = os.path.join(this_path, params['data_path'], f'qsc_out.random_scan_nfp{params["nfp"]}.csv')
-    df = pd.read_csv(filename)
+    output_shape = Y_train.shape[1]
 
-    x_columns = [col for col in df.columns if col.startswith('x')]
-    y_columns = [col for col in df.columns if col.startswith('y')]
+    return X_train, X_test, input_shape, output_shape, scaler_x
 
-    Y = df[x_columns].values
-    X = df[y_columns].values
+def create_dataset(X_train, Y_train, params):
+    train_dataset = tf.data.Dataset.from_tensor_slices((X_train, Y_train))
+    train_dataset = train_dataset.shuffle(buffer_size=1024).batch(params['batch_size']).prefetch(tf.data.experimental.AUTOTUNE)
 
-    k = 5
-    kfold = KFold(n_splits=k, shuffle=True, random_state=42)
+    return train_dataset
 
-    metrics_list = []
-    losses_list = []
+if len(sys.argv) > 1:
+    params['nfp'] = int(sys.argv[1])
 
-    for fold, (train_indices, test_indices) in enumerate(kfold.split(X, Y)):
-        X_train, X_test = X[train_indices], X[test_indices]
-        Y_train, Y_test = Y[train_indices], Y[test_indices]
+this_path = str(Path(__file__).parent.resolve())
+general_results_path = os.path.join(this_path, params['results_path'])
+results_path = os.path.join(general_results_path, f'nfp{params["nfp"]}')
+os.makedirs(results_path, exist_ok=True)
 
-        scaler_x = StandardScaler().fit(X_train)
-        X_train = scaler_x.transform(X_train)
-        X_test = scaler_x.transform(X_test)
+filename = os.path.join(this_path, params['data_path'], f'qsc_out.random_scan_nfp{params["nfp"]}.csv')
+df = pd.read_csv(filename)
 
-        if params['model'] == 'cnn':
-            X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
-            X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
-            input_shape = (X_train.shape[1], 1)
-            output_shape = Y_train.shape[1]
-            model = build_cnn(input_shape, output_shape)
-        else:
-            input_shape = X_train.shape[1]
-            output_shape = Y_train.shape[1]
-            model = build_neural_network(input_shape, output_shape)
+x_columns = [col for col in df.columns if col.startswith('x')]
+y_columns = [col for col in df.columns if col.startswith('y')]
 
-        learning_rate = tf.keras.optimizers.schedules.ExponentialDecay(
-            initial_learning_rate=params['learning_rate'], decay_steps=1000, decay_rate=0.9
-        )
+Y = df[x_columns].values
+X = df[y_columns].values
 
-        model.compile(optimizer=params['optimizer'](learning_rate=learning_rate), loss=Huber(), metrics=['mae'])
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=params['test_size'], random_state=params['random_state'])
 
-        early_stopping = EarlyStopping(monitor='val_loss', patience=params['early_stopping_patience'], restore_best_weights=True)
-        model_checkpoint = ModelCheckpoint(os.path.join(results_path, f"nn_qsc_nfp{params['nfp']}_fold{fold + 1}_best.h5"), monitor='val_loss', save_best_only=True)
+X_train, X_test, input_shape, output_shape, scaler_x = preprocess_data(X_train, X_test, Y_train, Y_test, params)
 
-        model.fit(X_train, Y_train, epochs=params['epochs'], batch_size=params['batch_size'], validation_split=0.2, verbose=1, callbacks=[early_stopping, model_checkpoint])
+if params['model'] == 'cnn':
+    model = build_cnn(input_shape, output_shape)
+else:
+    model = build_neural_network(input_shape, output_shape)
 
-        loss, metric = model.evaluate(X_test, Y_test, verbose=0)
-        metrics_list.append(metric)
-        losses_list.append(loss)
+learning_rate = tf.keras.optimizers.schedules.ExponentialDecay(
+    initial_learning_rate=params['learning_rate'], decay_steps=1000, decay_rate=0.9
+)
 
-        predictions = model(X_test)
+model.compile(optimizer=params['optimizer'](learning_rate=learning_rate), loss=Huber(), metrics=['mae'])
 
-        plt.figure(figsize=(8, 8))
-        plt.scatter(Y_test, predictions, label='Predicted values', alpha=0.5)
-        min_x = np.min([Y_test, predictions])
-        max_x = np.max([Y_test, predictions])
-        plt.plot([min_x, max_x], [min_x, max_x], 'r', label='Perfect predictions')
-        plt.xlabel('True values')
-        plt.ylabel('Predicted values')
-        plt.title(f'Fold {fold + 1}, nfp={params["nfp"]}, model={params["model"]}, metric={metric:.3f}, loss={loss:.3f}, epochs={params["epochs"]}, batch_size={params["batch_size"]}')
-        plt.legend()
-        plt.grid(True)
-        plt.savefig(os.path.join(results_path, f"nn_qsc_fold{fold + 1}_plot{params['nfp']}_model{params['model']}.png"))
-        plt.show()
+early_stopping = EarlyStopping(monitor='val_loss', patience=params['early_stopping_patience'], restore_best_weights=True)
+model_checkpoint = ModelCheckpoint(os.path.join(results_path, f"nn_qsc_nfp{params['nfp']}_best.h5"), monitor='val_loss', save_best_only=True)
 
-    avg_metric = np.mean(metrics_list)
-    avg_loss = np.mean(losses_list)
-    print(f"Average metric: {avg_metric}")
-    print(f"Average loss: {avg_loss}")
-    # Save the average metric and loss to a text file
-    with open(os.path.join(results_path, f"nn_qsc_nfp{params['nfp']}_summary.txt"), 'w') as summary_file:
-        summary_file.write(f"Average metric: {avg_metric}\n")
-        summary_file.write(f"Average loss: {avg_loss}\n")
+train_dataset = create_dataset(X_train, Y_train, params)
+validation_dataset = create_dataset(X_test, Y_test, params)
 
-    # Save the model and scaler to files
-    model.save(os.path.join(results_path, f"nn_qsc_nfp{params['nfp']}.h5"))
-    joblib.dump(scaler_x, os.path.join(results_path, f"nn_qsc{params['nfp']}.pkl"))
-    
-if __name__ == 'main':
-    main()
+model.fit(train_dataset, epochs=params['epochs'], validation_data=validation_dataset, verbose=1, callbacks=[early_stopping, model_checkpoint])
+
+loss, metric = model.evaluate(X_test, Y_test, verbose=0)
+
+predictions = model(X_test)
+
+plt.figure(figsize=(8, 8))
+plt.scatter(Y_test, predictions, label='Predicted values', alpha=0.5)
+min_x = np.min([Y_test, predictions])
+max_x = np.max([Y_test, predictions])
+plt.plot([min_x, max_x], [min_x, max_x], 'r', label='Perfect predictions')
+plt.xlabel('True values')
+plt.ylabel('Predicted values')
+plt.title(f'nfp={params["nfp"]}, model={params["model"]}, metric={metric:.3f}, loss={loss:.3f}, epochs={params["epochs"]}, batch_size={params["batch_size"]}')
+plt.legend()
+plt.grid(True)
+plt.savefig(os.path.join(results_path, f"nn_qsc_nfp{params['nfp']}_model{params['model']}.png"))
+plt.show()
+
+# Save the model and scaler
+model.save(os.path.join(results_path, f"nn_qsc_nfp{params['nfp']}_model{params['model']}.h5"))
+joblib.dump(scaler_x, os.path.join(results_path, f"nn_qsc_nfp{params['nfp']}_scaler_x.pkl"))
+print(f"Model and scaler saved in {results_path}")
