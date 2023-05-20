8e89f9b77ae7d67e1457d72d5de84f709b7e1be9
Rogerio Jorge <rogerio.jorge@ist.utl.pt>
2023-05-20 17:12:32 +0100

    Entropy results now there. Results not encouraging

--- stel_candidates_distribution.py
+++ stel_candidates_distribution.py
@@ -3,15 +3,15 @@ import numpy as np
 import pandas as pd
 from sklearn.mixture import GaussianMixture
 from sklearn.model_selection import train_test_split
-from scipy.special import kl_div
-from keras.models import load_model
-from pathlib import Path
 from sklearn.metrics import mean_absolute_error
 from sklearn.neighbors import KernelDensity
+from keras.models import load_model
+from pathlib import Path
 import joblib
 import matplotlib.pyplot as plt
 from scipy.stats import entropy
-
+from scipy.stats import zscore
+from scipy.stats import iqr
 
 params = {
     'results_path': 'results',
@@ -26,10 +26,8 @@ params = {
     'n_samples_subset': 400,
 }
 
-
 def get_path(name):
     return Path(params['results_path'], f"nfp{params['nfp']}", f"nn_qsc_nfp{params['nfp']}_{name}")
-
 
 def load_data(path: str):
     df = pd.read_parquet(path).apply(pd.to_numeric, downcast='float', errors='ignore')
@@ -37,28 +35,35 @@ def load_data(path: str):
     df = df.nsmallest(params['n_data_subset'], 'ysum').drop(columns='ysum')
     return train_test_split(df.filter(like='y').values, df.filter(like='x').values, test_size=params['test_size'], random_state=params['random_state'], shuffle=True)
 
+def optimal_bandwidth(data):
+    return 1.06 * np.std(data) * len(data)**(-1/5)
 
 def calculate_kl(p, q):
     return entropy(p, q)
 
-
 def fit_gmm(data):
     return GaussianMixture(n_components=params['n_components']).fit(data)
-
 
 def get_samples(data, num_samples):
     return np.random.choice(data.flatten(), num_samples)
 
-
 def calculate_mae(y_true, y_pred):
     return mean_absolute_error(y_true, y_pred)
 
+def remove_outliers(X, Y):
+    z_scores = zscore(X)
+    abs_z_scores = np.abs(z_scores)
+    filtered_entries = (abs_z_scores < 3).all(axis=1)
+    return X[filtered_entries], Y[filtered_entries]
 
-def plot_graphs(bins, data_subset, samples_subset, kl_divergences):
-    kde_data = KernelDensity(bandwidth=1.0, kernel='gaussian').fit(data_subset[:, None])
+def plot_graphs(bins, data_subset, samples_subset, kl_divergences, results_path, plot_name):
+    bandwidth_data = optimal_bandwidth(data_subset)
+    bandwidth_samples = optimal_bandwidth(samples_subset)
+
+    kde_data = KernelDensity(bandwidth=bandwidth_data, kernel='gaussian').fit(data_subset[:, None])
+    kde_samples = KernelDensity(bandwidth=bandwidth_samples, kernel='gaussian').fit(samples_subset[:, None])
+
     logprob_data = kde_data.score_samples(bins[:, None])
-
-    kde_samples = KernelDensity(bandwidth=1.0, kernel='gaussian').fit(samples_subset[:, None])
     logprob_samples = kde_samples.score_samples(bins[:, None])
 
     gmm_data = GaussianMixture(n_components=2).fit(data_subset[:, None])
@@ -73,7 +78,7 @@ def plot_graphs(bins, data_subset, samples_subset, kl_divergences):
     midpoints_data = (bin_edges_data[:-1] + bin_edges_data[1:]) / 2
     midpoints_samples = (bin_edges_samples[:-1] + bin_edges_samples[1:]) / 2
 
-    plt.figure(figsize=(12, 8))
+    plt.figure(figsize=(5, 8))
 
     plt.subplot(311)
     plt.plot(midpoints_data, freq_data, label='Data')
@@ -94,52 +99,53 @@ def plot_graphs(bins, data_subset, samples_subset, kl_divergences):
     plt.legend()
 
     plt.tight_layout()
+    plt.savefig(os.path.join(results_path, f'{plot_name}.png'), dpi=200)
     plt.show()
 
 if __name__ == "__main__":
     this_path = os.path.abspath('')
+    general_results_path = os.path.join(this_path, params['results_path'])
+    results_path = os.path.join(general_results_path, f'nfp{params["nfp"]}')
+    os.makedirs(results_path, exist_ok=True)
     filename = os.path.join(this_path, params['data_path'], f'qsc_out.random_scan_nfp{params["nfp"]}.parquet')
+
     X_train, X_test, Y_train, Y_test = load_data(filename)
-    
-    gmm_all = fit_gmm(np.concatenate((X_train, Y_train), axis=1))
-    gmm_input = fit_gmm(X_train)
-    
+    X_train, Y_train = remove_outliers(X_train, Y_train)
+    X_test, Y_test   = remove_outliers(X_test, Y_test)
+
     model = load_model(get_path(f'model{params["model"]}.h5'))
     scaler_x = joblib.load(get_path('scaler_x.pkl'))
     scaler_y = joblib.load(get_path('scaler_y.pkl'))
 
-    samples_all = gmm_all.sample(params['n_samples'])[0][:, :X_train.shape[1]]
+    predictions_test = model.predict(X_test)
+    mae_test = calculate_mae(scaler_y.inverse_transform(predictions_test), Y_test)
+    print(f'MAE for test set: {mae_test:.2f}')
+
+    gmm_input = fit_gmm(X_train)
     samples_input = gmm_input.sample(params['n_samples'])[0]
-    
-    predictions_all = model.predict(samples_all)
     predictions_input = model.predict(samples_input)
 
-    # Ensure the shapes of predictions and Y_test subsets match
-    if predictions_all.shape[0] != Y_test.shape[0]:
-        predictions_all = predictions_all[:Y_test.shape[0], :]
-    if predictions_input.shape[0] != Y_test.shape[0]:
-        predictions_input = predictions_input[:Y_test.shape[0], :]
-    
-    Y_test_subset_all = Y_test[:predictions_all.shape[0], :]
-    mae_all = calculate_mae(scaler_y.inverse_transform(predictions_all), Y_test_subset_all)
+    min_val = min(np.min(Y_test), np.min(predictions_input))
+    max_val = max(np.max(Y_test), np.max(predictions_input))
 
-    Y_test_subset_input = Y_test[:predictions_input.shape[0], :]
-    mae_input = calculate_mae(scaler_y.inverse_transform(predictions_input), Y_test_subset_input)
+    margin = 0.05 * (max_val - min_val)  # 5% of the range
+    bins = np.linspace(min_val - margin, max_val + margin, 100)
 
-    bins = np.linspace(-10, 10, 100)
-    kl_divergences_all = []
     kl_divergences_input = []
 
     for idx in range(X_train.shape[1]):
-        data_subset_all = get_samples(Y_test[:, idx], params['n_samples_subset'])
-        samples_subset_all = predictions_all[:params['n_samples_subset'], idx]
-        kl_divergence_all = calculate_kl(data_subset_all, samples_subset_all)
-        kl_divergences_all.append(kl_divergence_all)
-        
         data_subset_input = get_samples(Y_test[:, idx], params['n_samples_subset'])
         samples_subset_input = predictions_input[:params['n_samples_subset'], idx]
-        kl_divergence_input = calculate_kl(data_subset_input, samples_subset_input)
+
+        p_data, _ = np.histogram(data_subset_input, bins=bins, density=True)
+        p_samples, _ = np.histogram(samples_subset_input, bins=bins, density=True)
+
+        kl_divergence_input = calculate_kl(p_data, p_samples)
         kl_divergences_input.append(kl_divergence_input)
 
-    plot_graphs(bins, data_subset_all, samples_subset_all, kl_divergences_all)
-    plot_graphs(bins, data_subset_input, samples_subset_input, kl_divergences_input)
+    print('Finished KL divergence calculations.')
+    
+    plot_graphs(bins, data_subset_input, samples_subset_input, kl_divergences_input, results_path, 'kl_input_results')
+    print('Plotted and saved figures for input results.')
+    print('Finished all calculations and visualizations.')
+
